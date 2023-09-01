e6041378e2608e0b1676380d9694fb7e506a7a26
anasjawanpakistan <anasm0213@gmail.com>
2023-09-01 21:22:11 +0500

    add

---
+++ CLASS  01-SEP-2023/app.js
@@ -0,0 +1,26 @@
+alert("java Script")
+
+document.write("Welcome to Java Script")
+
+console.log("Welcome to the basic Java Script or Vanilla Java Script")
+
+
+var number = 50;
+
+
+var numberone;
+
+numberone = 51
+
+console.log(number)
+
+alert(numberone)
+
+
+
+var firstname= ("Muhammad")
+var lastname= ("Hamza")
+
+console.log(firstname)
+console.log(lastname)
+
